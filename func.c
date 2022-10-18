523460e85f369cda93cc20edf911909791c8320e
benvarian <88700858+benvarian@users.noreply.github.com>
2022-10-18 15:39:18 +0800

    added pipe functionality

--- func.c
+++ func.c
@@ -2,20 +2,53 @@
 
 void regex(char *fn)
 {
-    char *file = "grep";
-    char *arg1 = "-aoE";
+    char *cmd = "grep";
+    char *arg1 = "-raoE";
     char *arg2 = "--null-data";
     char *regexPattern = "\\b\\w{4,}\\b";
 
     // HASHTABLE *h = hashtable_new();
-    printf("%s\n", fn);
-    if (fork() == 0)
+    // printf("%s\n", fn);
+    // if (fork() == 0)
+    // {
+    //     if (execlp(file, file, arg1, arg2, regexPattern, fn, (char *)NULL) == -1)
+    //         perror("error");
+    // }
+    int pid = fork();
+    if (pid == -1)
     {
-        if (execlp(file, file, arg1, arg2, regexPattern, fn, (char *)NULL) == -1)
-            perror("error");
+        perror("error");
+        // return 1;
     }
-
-    
+    if (pid == 0)
+    {
+        int file = open("results.txt", O_WRONLY | O_CREAT, 0777);
+        if (file == -1)
+        {
+            perror("err");
+        }
+        dup2(file, STDOUT_FILENO);
+        close(file);
+        int err = execlp(cmd, cmd, arg1, arg2, regexPattern, fn, (char *)NULL);
+        if (err == -1)
+        {
+            perror("couldnt run");
+        }
+    }
+    else
+    {
+        int wstatus;
+        wait(&wstatus);
+        if (WIFEXITED(wstatus))
+        {
+            int statuscode = WEXITSTATUS(wstatus);
+            if (statuscode != 0)
+            {
+                perror("error");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
 void myPrint(void)
 {
